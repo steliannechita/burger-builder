183ed22603fc8cabba51558b5dda46f03a8dc75a
stelian nechita <steliannechita2013@gmail.com>
2019-09-13 10:07:38 +0100

    first commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+import styles from "./Layout.module.css";
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+const layout = props => (
+  <Auxiliary>
+
+    <Toolbar/>
+    <main className={styles.Content}>{props.children}</main>
+  </Auxiliary>
+);
+
+export default layout;
